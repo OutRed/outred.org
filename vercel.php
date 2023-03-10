85833c8db067f1b291e9424a65857eaa0a494b4c
OutRed <103283478+OutRed@users.noreply.github.com>
2023-03-09 20:08:48 -0600

    Delete vercel.php

--- vercel.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url = $_GET['url'];
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$content = curl_exec($ch);
-$content = str_replace($url, $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], $content);
-echo $content;
