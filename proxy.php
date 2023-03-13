4ce323bb02cd2c57881dce23fcd49e7250fc9900
OutRed <103283478+OutRed@users.noreply.github.com>
2023-03-13 16:06:02 -0500

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,11 +1,31 @@
 <?php
 if(isset($_POST['url'])) {
+    // Get the URL to be proxied
     $url = $_POST['url'];
-    $html = file_get_contents($url);
-    $html = str_replace('href="http', 'href="/proxy.php?url=http', $html);
-    $html = str_replace('src="http', 'src="/proxy.php?url=http', $html);
-    echo $html;
-} else {
-    echo 'Enter a URL to proxy.';
+
+    // Open the cURL session
+    $curlSession = curl_init();
+
+    // Set the URL of the page to be retrieved
+    curl_setopt($curlSession, CURLOPT_URL, $url);
+
+    // Set cURL options
+    curl_setopt($curlSession, CURLOPT_HEADER, false);
+    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curlSession, CURLOPT_FOLLOWLOCATION, true);
+
+    // Retrieve the page
+    $response = curl_exec($curlSession);
+
+    // Close the cURL session
+    curl_close($curlSession);
+
+    // Replace all absolute URLs with relative URLs
+    $base_url = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+    $response = str_replace($url, '', $response);
+    $response = str_replace($base_url, '', $response);
+
+    // Output the response
+    echo $response;
 }
 ?>
