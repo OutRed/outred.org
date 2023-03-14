7dd95cab7931a6d9f15a5b7836b000df5b137460
OutRed <103283478+OutRed@users.noreply.github.com>
2023-03-13 20:47:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// For the proxy test, connected to proxy.html
 function loadUrl() {
   var url = document.getElementById("urlInput").value;
   document.getElementById("proxyFrame").src = "https://cors-anywhere.herokuapp.com/" + url;
